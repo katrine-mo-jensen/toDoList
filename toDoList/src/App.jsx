d00c3eec852fd33d7ac8a080e07fe805f70db0f5
katrine-mo-jensen <katrine.mo.jensen@gmail.com>
2023-08-17 13:23:56 +0200

    hh

--- toDoList/src/App.jsx
+++ toDoList/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.scss'
 import { Header } from './components/Header/Header'
+import { AddTodo } from './components/toDoList/ToDoList'
 
 function App() {
   
   return (
     <div className='appContainer'>
       <Header />
+      <AddTodo/>
     </div>
   )
 }
