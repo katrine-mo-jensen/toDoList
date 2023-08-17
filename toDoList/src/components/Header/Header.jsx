4a0bb18422caa0097980c20a15ed2785e3b0d39f
Kamilla <113370998+Kanin60@users.noreply.github.com>
2023-08-17 13:17:14 +0200

    header done

--- toDoList/src/components/Header/Header.jsx
+++ toDoList/src/components/Header/Header.jsx
@@ -1,2 +1,10 @@
 import style from './Header.module.scss'
 
+export const Header = () => {
+
+    return(
+        <header className={style.Header}>
+            <h1>TO DO</h1>
+        </header>
+    )
+}
