31f8c5a7af2a14f6b8a0a4bef3d865225eeffc6f
Kamilla <113370998+Kanin60@users.noreply.github.com>
2023-08-17 13:01:07 +0200

    Iconer

--- toDoList/src/App.jsx
+++ toDoList/src/App.jsx
@@ -1,11 +1,11 @@
-
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
   
   return (
     <div className='appContainer'>
-
+      <Header />
     </div>
   )
 }
