2fecf7e03cc3b6b94f3e7d7dc687bc1966991513
Kamilla <113370998+Kanin60@users.noreply.github.com>
2023-08-18 10:38:14 +0200

    Add til todo
    Add til todo

--- toDoList/src/App.jsx
+++ toDoList/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.scss'
 import { Header } from './components/Header/Header'
+import { ToDoList } from './components/toDoList/ToDoList'
 
 function App() {
   
   return (
     <div className='appContainer'>
       <Header />
+      <ToDoList />
     </div>
   )
 }
--- toDoList/src/components/toDoList/ToDoList.jsx
+++ toDoList/src/components/toDoList/ToDoList.jsx
@@ -1,10 +1,25 @@
 import { useState } from "react"; 
 
 export function ToDoList() {
+    const [todoArray, setToDoArray] = useState(['', ''])
+
+    const [name, setName] = useState('')
+
+    const addToArray = () => {
+        let clone = [...todoArray]
+        clone.push(name)
+        console.log(clone);
+        setToDoArray(clone)
+    }
+
 
     return(
         <>
-        
+        {todoArray.map((item, index)=>
+            <p key={index}>{item}</p>
+        )}
+        <input placeholder={"Skriv dit navn her"} value={name} onChange={(event) => setName(event.target.value)} />
+        <button onClick={addToArray}>Add To Array</button>
         </>
     )
 
