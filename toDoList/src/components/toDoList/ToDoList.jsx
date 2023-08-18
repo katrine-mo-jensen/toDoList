b258768ff7c26530abc0c9b9d4f48fe0a4a0d282
katrine-mo-jensen <katrine.mo.jensen@gmail.com>
2023-08-18 13:16:21 +0200

    so far

--- toDoList/src/components/toDoList/ToDoList.jsx
+++ toDoList/src/components/toDoList/ToDoList.jsx
@@ -1,36 +1,44 @@
-import { useState } from "react"; 
+import { useState } from "react";
+import style from "./toDoList.module.scss";
+import plus from '../../assets/plus.png'
+import minus from '../../assets/minus.png'
 
 export function ToDoList() {
-    const [todoArray, setToDoArray] = useState(['opgave 1', 'Opgave 2'])
+  const [todoArray, setToDoArray] = useState(["Køb havremælk", "Lav SCSS færdig"]);
 
-    const [name, setName] = useState('')
+  const [name, setName] = useState("");
 
-    const addToArray = () => {
-        let clone = [...todoArray]
-        clone.push(name)
-        console.log(clone);
-        setToDoArray(clone)
-    }
+  const addToArray = () => {
+    let clone = [...todoArray];
+    clone.push(name);
+    console.log(clone);
+    setToDoArray(clone);
+  };
 
-    const removeFromArray = () => {
-        let slet = [...todoArray]
-        slet.pop(name)
-        console.log(slet);
-        setToDoArray(slet)
-    }
+  const removeFromArray = () => {
+    let slet = [...todoArray];
+    slet.pop(name);
+    console.log(slet);
+    setToDoArray(slet);
+  };
 
-    return(
-        <>
-            {todoArray.map((item, index)=>
-                <div key={index}>
-                    <p >{item}</p>
-                    <button onClick={removeFromArray}>Delete</button>
-                </div>
-            )}
-            <input placeholder={"Tilføj en opgave"} value={name} onChange={(event) => setName(event.target.value)} />
-            <button onClick={addToArray}>Tilføj</button>
-        </>
-    )
+  return (
+    <section className={style.toDo}>
+      {todoArray.map((item, index) => (
+        <div className={style.theList} key={index}>
+          <p>{item}</p>
+          <button onClick={removeFromArray}> <img src={minus} alt="" /> </button>
+        </div>
+      ))}
 
-
+      <div className={style.inputAndAdd}>
+      <input
+        placeholder={"Tilføj en opgave"}
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+      />
+      <button className="addNew" onClick={addToArray}><img src={plus} alt="" /></button>
+      </div>
+    </section>
+  );
 }
